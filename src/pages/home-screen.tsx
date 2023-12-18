12f87b2e7174ea1051e4769a33aeff8fe8390a7d
Mateus Hilario Dias <118845101+mateushilariodiasnh@users.noreply.github.com>
2023-12-18 14:01:10 -0300

    Update home-screen.tsx

--- src/pages/home-screen.tsx
+++ src/pages/home-screen.tsx
@@ -7,14 +7,23 @@ import { useState } from 'react';
 
 export default function HomeScreen() {
 
-  const [openModal, setOpenModal] = useState(false);
+  const [openModalCreate, setOpenModalCreate] = useState(false);
+  const [openModallView, setOpenModallView] = useState(false);
 
-  const openToModal = () => (
-    setOpenModal(true)
+  const openToModalCreate = () => (
+    setOpenModalCreate(true)
   )
 
-  const closeModal = () => (
-    setOpenModal(false)
+  const closeModalCreate = () => (
+    setOpenModalCreate(false)
+  )
+
+  const openToModalView = () => (
+    setOpenModallView(true)
+  )
+
+  const closeModalView = () => (
+    setOpenModallView(false)
   )
 
   return (
@@ -31,13 +40,13 @@ export default function HomeScreen() {
         <div className='max-w-full mr-36 ml-19'>
           <p className="text-center text-mediumToneBlack font-bold text-4.4xl py-16">Trabalhos</p>
         </div>
-        <img src="/images/Plus.svg" alt="Icon Plus" className='pb-9' onClick={openToModal}/>
+        <img src="/images/Plus.svg" alt="Icon Plus" className='pb-9' onClick={openToModalCreate} />
         <section className='bg-whiteBlueGray max-w-full mr-36 ml-19 flex justify-between'>
           <div className='align-middle ml-20 text-mediumToneBlack'>
             <p className='pt-11 font-bold text-4.4xl'>Titulo</p>
             <p className='pb-11 font-normal text-3.1xl'>Aluno 1, Aluno 2</p>
           </div>
-          <div className='bg-mediumToneBlue p-3 rounded-md h-min w-min my-auto mr-7'>
+          <div className='bg-mediumToneBlue p-3 rounded-md h-min w-min my-auto mr-7' onClick={openToModalView}>
             <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" viewBox="0 0 51 51" fill="none">
               <g clipPath="url(#clip0_222_443)">
                 <path d="M25.2516 9.875C14.835 9.875 5.93913 16.3542 2.33496 25.5C5.93913 34.6458 14.835 41.125 25.2516 41.125C35.6683 41.125 44.5641 34.6458 48.1683 25.5C44.5641 16.3542 35.6683 9.875 25.2516 9.875ZM25.2516 35.9167C19.5016 35.9167 14.835 31.25 14.835 25.5C14.835 19.75 19.5016 15.0833 25.2516 15.0833C31.0016 15.0833 35.6683 19.75 35.6683 25.5C35.6683 31.25 31.0016 35.9167 25.2516 35.9167ZM25.2516 19.25C21.7933 19.25 19.0016 22.0417 19.0016 25.5C19.0016 28.9583 21.7933 31.75 25.2516 31.75C28.71 31.75 31.5016 28.9583 31.5016 25.5C31.5016 22.0417 28.71 19.25 25.2516 19.25Z" fill="#F3F3F3" />
@@ -50,39 +59,74 @@ export default function HomeScreen() {
             </svg>
           </div>
         </section>
-        {openModal && (  
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-40">
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
-        <section className='mt-10 bg-whiteBlueGray max-w-3.5xl pb-12 rounded-10rem'>
-          <p className='text-5xl text-center font-bold pt-20 pb-10'>Informações básicas</p>
-          <div className='mx-16'>
-            <label htmlFor="title" className='font-bold text-2xl'>Título do trabalho</label><br />
-            <Input type="text" name="title" id="title" className='mt-2 mb-5'/>
-          </div>
-          <div className='mx-16'>
-            <label htmlFor="firstStudent" className='font-bold text-2xl'>Nome do aluno 1</label><br />
-            <Input type="text" name="firstStudent" id="firstStudent" className='mt-2 mb-5'/>
-          </div>
-          <div className='mx-16'>
-            <label htmlFor="secondStudent" className='font-bold text-2xl'>Nome do aluno 2</label><br />
-            <Input type="text" name="secondStudent" id="secondStudent" className='mt-2 mb-5'/>
-          </div>
-          <div className='mx-16'>
-            <label htmlFor="goal" className='font-bold text-2xl'>Objetivo</label><br />
-            <Input type="text" name="goal" id="goal" className='mt-2 mb-5'/>
-          </div>
-          <div className='mx-16 mb-9'>
-            <label htmlFor="" className='font-bold text-2xl'>Estimativa de prazo</label>
-            <div className='flex flex-row gap-5 mt-2'>
-            <Input type="text" name="initialDate" id="initialDate" />
-            <Input type="text" name="finalDate" id="finalDate" />
+        {openModalCreate && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-40">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+              <section className='bg-whiteBlueGray max-w-3.5xl pb-12 rounded-10rem'>
+                <p className='text-5xl text-center font-bold pt-20 pb-10'>Informações básicas</p>
+                <div className='mx-16'>
+                  <label htmlFor="title" className='font-bold text-2xl'>Título do trabalho</label><br />
+                  <Input type="text" name="title" id="title" className='mt-2 mb-5' />
+                </div>
+                <div className='mx-16'>
+                  <label htmlFor="firstStudent" className='font-bold text-2xl'>Nome do aluno 1</label><br />
+                  <Input type="text" name="firstStudent" id="firstStudent" className='mt-2 mb-5' />
+                </div>
+                <div className='mx-16'>
+                  <label htmlFor="secondStudent" className='font-bold text-2xl'>Nome do aluno 2</label><br />
+                  <Input type="text" name="secondStudent" id="secondStudent" className='mt-2 mb-5' />
+                </div>
+                <div className='mx-16'>
+                  <label htmlFor="goal" className='font-bold text-2xl'>Objetivo</label><br />
+                  <Input type="text" name="goal" id="goal" className='mt-2 mb-5' />
+                </div>
+                <div className='mx-16 mb-9'>
+                  <label htmlFor="" className='font-bold text-2xl'>Estimativa de prazo</label>
+                  <div className='flex flex-row gap-5 mt-2'>
+                    <Input type="text" name="initialDate" id="initialDate" />
+                    <Input type="text" name="finalDate" id="finalDate" />
+                  </div>
+                </div>
+                <Button href='' className='mx mx-16'>
+                  <span className='inline-flex mx-auto py-6' onClick={closeModalCreate} >Cadastrar</span>
+                </Button>
+              </section>
             </div>
           </div>
-          <Button href='' className='mx mx-16'>
-            <span className='inline-flex mx-auto py-6' onClick={closeModal} >Cadastrar</span>
-          </Button>
-        </section>
-        </div>
+        )}
+        {openModallView && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-40">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+              <section className='bg-whiteBlueGray max-w-3.5xl pb-12 rounded-10rem px-12'>
+                <p className='pt-14 pb-4 font-bold text-2.1xl text-mediumToneBlack'>Título</p>
+                <p className='pb-4 font-semibold text-base2'>Aluno 1, Aluno 2</p>
+                <p className='pb-4 font-normal text-base2'>Objetivo</p>
+                <p className='pb-5 font-normal text-base2'>15/06/2023 · 15/06/2024</p>
+                <div className='flex flex-row pb-5'>
+                  <div className='py-1 px-3 bg-mediumToneWhite rounded-10rem mr-3'>
+                    <p>Marcelo</p>
+                  </div>
+                  <div className='py-1 px-3 bg-mediumToneWhite rounded-10rem mr-3'>
+                    <p>Adriana</p>
+                  </div>
+                  <div className='py-1 px-3 bg-mediumToneWhite rounded-10rem'>
+                    <p>Claudio</p>
+                  </div>
+                </div>
+                <div className='h-1 bg-primaryDarkBlue'>
+                </div>
+                <div className='pt-6 pb-5 flex flex-row justify-between'>
+                  <span>Ver mais detalhes...</span>
+                  <img src="/images/Expand-more.svg" alt="Expand more" />
+                </div>
+                <div className='flex justify-end'>
+                  <Link href='' className='flex justify-center gap-1 bg-mediumToneBlue rounded-10rem py-2 px-8 items-center'>
+                    <img src="/images/Print.svg" alt="Print" />
+                    <span className='font-medium text-1.5xl text-whiteBlueGray '>Exportar</span>
+                  </Link>
+                </div>
+              </section>
+            </div>
           </div>
         )}
       </main>
