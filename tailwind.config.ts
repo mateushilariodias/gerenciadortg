9ca8cd0ef095cc401574961bd2d36f0fef06db3d
Mateus Hilario Dias <118845101+mateushilariodiasnh@users.noreply.github.com>
2023-12-18 14:02:12 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,26 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        primaryDarkBlue: '#454C54',
         secondDarkBlue: '#6FA0C6',
         mediumToneBlue: '#3B6E96',
         whiteBlueGray: '#E1E7F6',
         mediumToneBlack: '#1E292C',
+        mediumToneWhite: '#F8F9FE',
       },
       borderRadius: {
         '10rem': '0.625rem',
       },
       fontSize: {
+        'base2': [ '1rem', {
+          lineHeight: '1rem'
+        }],
+        '1.5xl': [ '1.25rem', {
+          lineHeight: '1.25rem'
+        }],
+        '2.1xl': [ '1.5rem', {
+          lineHeight: '1.5rem'
+        }],
         '3.1xl': [ '1.875rem', {
           lineHeight: '1.938rem'
         }],
