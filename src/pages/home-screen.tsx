7414c8a572791745eb0b8e6630f6d0d13d5f788d
mateushilariodias <mateusilariodias@gmail.com>
2023-12-19 22:17:10 -0300

    create image svg

--- src/pages/home-screen.tsx
+++ src/pages/home-screen.tsx
@@ -46,18 +46,7 @@ export default function HomeScreen() {
             <p className='pt-11 font-bold text-4.4xl'>Titulo</p>
             <p className='pb-11 font-normal text-3.1xl'>Aluno 1, Aluno 2</p>
           </div>
-          <div className='bg-mediumToneBlue p-3 rounded-md h-min w-min my-auto mr-7' onClick={openToModalView}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="51" height="51" viewBox="0 0 51 51" fill="none">
-              <g clipPath="url(#clip0_222_443)">
-                <path d="M25.2516 9.875C14.835 9.875 5.93913 16.3542 2.33496 25.5C5.93913 34.6458 14.835 41.125 25.2516 41.125C35.6683 41.125 44.5641 34.6458 48.1683 25.5C44.5641 16.3542 35.6683 9.875 25.2516 9.875ZM25.2516 35.9167C19.5016 35.9167 14.835 31.25 14.835 25.5C14.835 19.75 19.5016 15.0833 25.2516 15.0833C31.0016 15.0833 35.6683 19.75 35.6683 25.5C35.6683 31.25 31.0016 35.9167 25.2516 35.9167ZM25.2516 19.25C21.7933 19.25 19.0016 22.0417 19.0016 25.5C19.0016 28.9583 21.7933 31.75 25.2516 31.75C28.71 31.75 31.5016 28.9583 31.5016 25.5C31.5016 22.0417 28.71 19.25 25.2516 19.25Z" fill="#F3F3F3" />
-              </g>
-              <defs>
-                <clipPath id="clip0_222_443">
-                  <rect width="50" height="50" fill="white" transform="translate(0.25 0.5)" />
-                </clipPath>
-              </defs>
-            </svg>
-          </div>
+           <img src="/images/Remove-red-eye.svg" alt="Remove red eye" className='bg-mediumToneBlue p-3 rounded-md h-min w-min my-auto mr-7' onClick={openToModalView}/>
         </section>
         {openModalCreate && (
           <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-40">
